5c6aecd6bf05f7086ad6aed68c7bb4144274af75
danppo <danielpocock@gmail.com>
2023-03-08 20:12:27 +0000

    adding register

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,5 +1,8 @@
 import { ChangeEvent, useState } from 'react';
-import { Box, Input, Button, Stack } from '@chakra-ui/react';
+import { Box, Input, Button, Stack,   FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText, } from '@chakra-ui/react';
 import EmailInput from '../EmailInput';
 import { BeatLoader } from 'react-spinners';
 import Alerts from 'components/Alerts';
@@ -11,6 +14,31 @@ const Register = () => {
   return (
     <Box textAlign='center'>
       <Stack mx={8} spacing={3}>
+        <FormControl> 
+          <FormLabel>User name</FormLabel>
+          <Input variant='flushed' type='text' />
+          <FormHelperText>We'll never share your email.</FormHelperText>
+        </FormControl>
+        <FormControl> 
+          <FormLabel>Email address</FormLabel>
+          <Input variant='flushed' type='email' />
+          <FormHelperText>We'll never share your email.</FormHelperText>
+        </FormControl>
+        <FormControl> 
+          <FormLabel>password</FormLabel>
+          <Input variant='flushed' type='password' />
+          <FormHelperText>We'll never share your email.</FormHelperText>
+        </FormControl>
+        <FormControl> 
+          <FormLabel>meter number</FormLabel>
+          <Input variant='flushed' type='number' />
+          <FormHelperText>We'll never share your email.</FormHelperText>
+        </FormControl>
+        <FormControl> 
+          <FormLabel>postcode </FormLabel>
+          <Input variant='flushed' type='text' />
+          <FormHelperText>To get local weather temprature.</FormHelperText>
+        </FormControl>
         <EmailInput
           disabled={false}
           isInvalid={isEmailInvalid}
